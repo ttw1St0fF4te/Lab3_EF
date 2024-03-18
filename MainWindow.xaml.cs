4773bb38d37708443962b52b6a78337a17ebf101
ttw1St0fF4te <isip_e.e.novikov@mpt.ru>
2024-03-18 23:21:56 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab3_EF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MakdoknekEntities dazabannix = new MakdoknekEntities();
+        public MainWindow()
+        {
+            InitializeComponent();
+            ClientDgr.ItemsSource = dazabannix.Booking.ToList();
+        }
+    }
+}
